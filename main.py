486fd37283945b6238389607d0eaa7fa4d85587f
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2022-02-01 07:28:27 +0700

    Change commands prefix

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from nextcord.ext import commands
 from chat_response import get_response
 from dotenv import load_dotenv
 
-client = commands.Bot(command_prefix = '<usr')
+client = commands.Bot(command_prefix = '<')
 
 @client.event
 async def on_ready():
     print(f'Successfully logged in as: {client.user}')
     
 
-@client.command(name = '>')
+@client.command(name = 'usr>')
 async def answer(ctx, *msg):
     await ctx.send(get_response(' '.join(list(msg))))
         
