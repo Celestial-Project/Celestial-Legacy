fce6bbcf7336958fe4e198718220ef2d40f78b49
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2022-06-27 15:10:39 +0700

    fixing typo

--- cli_tester.py
+++ cli_tester.py
@@ -2,7 +2,7 @@ from chat_response import get_response
 
 # run this file to test your chat intents on the terminal before commit
 
-print('\u001b[45;1m ** \u001b[0m Welcome to Celestial commnad-line testing interface!')
+print('\u001b[45;1m ** \u001b[0m Welcome to Celestial command-line testing interface!')
 print('\u001b[45;1m ** \u001b[0m Press ctrl+c to exit.')
 
 while True:
