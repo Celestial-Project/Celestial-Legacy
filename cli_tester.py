ea22dda2482d6734cae52ac0672159f6e3732773
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2022-06-28 00:00:46 +0700

    Add "reload" macro on command line intents tester

--- cli_tester.py
+++ cli_tester.py
@@ -1,3 +1,5 @@
+import os 
+import sys
 from chat_response import get_response
 
 # run this file to test your chat intents on the terminal before commit
@@ -8,7 +10,17 @@ print('\u001b[45;1m ** \u001b[0m Press ctrl+c to exit.')
 while True:
     
     try:
-        get_response(input('\u001b[47;1m #> \u001b[0m '), debug = True)
+        
+        inp = input('\u001b[47;1m #> \u001b[0m ')
+        
+        # Reload the engine on command
+        if inp == '--reload' or inp == '-r':
+            os.system('clear')
+            print('\u001b[45;1m ** \u001b[0m Reloading...')
+            os.execl(sys.executable, sys.executable, *sys.argv)
+                  
+        else:
+            get_response(inp, debug = True)
         
     except KeyboardInterrupt:
         print('\n\u001b[45;1m ** \u001b[0m Exiting test mode...')
