1c674aacda000083db15f9eceb5c4d6f6257f40a
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2022-06-28 07:36:29 +0700

    Add reload command for the discord bot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from nextcord.ext import commands
 from chat_response import get_response
@@ -6,16 +7,32 @@ from dotenv import load_dotenv
 
 client = commands.Bot(command_prefix = '<')
 
+load_dotenv()
+
+modList = [int(os.getenv('ID1'))]
+
+def is_owner(ctx):
+    if ctx.author.id in modList:
+        return ctx.author.id in modList
+    
+
 @client.event
 async def on_ready():
-    print(f'Successfully logged in as: {client.user}')
+    os.system('clear')
+    print(f'\u001b[45;1m ** \u001b[0m Successfully logged in as: {client.user}')
     
 
 @client.command(name = 'usr>')
 async def answer(ctx, *msg):
     await ctx.send(get_response(' '.join(list(msg))))
+    
+    
+@client.command(name = 'reload>')
+@commands.check(is_owner)
+async def reload(ctx):
+    os.system('clear')
+    print('\u001b[45;1m ** \u001b[0m Reloading...')
+    os.execl(sys.executable, sys.executable, *sys.argv)
         
         
-load_dotenv()
-
 client.run(os.getenv('TOKEN'))
