24c738d7d7e55c9429d7407af821c4f7166cf164
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2022-07-20 07:09:04 +0700

    change message intents setting to new API change

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from nextcord.ext import commands
 from dotenv import load_dotenv
 from importlib import reload
 
+intents = nextcord.Intents.default()
+intents.message_content = True
+
 client = commands.Bot(command_prefix = '<')
 
 load_dotenv()
