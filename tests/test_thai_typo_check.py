ebe13eff5dd3c2f48938fbbc4cd8de5b026bbbae
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2022-08-09 00:07:25 +0700

    Change typo checking condition

--- tests/test_thai_typo_check.py
+++ tests/test_thai_typo_check.py
@@ -7,4 +7,4 @@ with open('./responses/responses_th.json') as f:
 
 def test_typo_check():
     for response in res_th:
-        assert [res for res in res_th[response]['response'] if res.endswith('นะค่ะ')] == []
+        assert [res for res in res_th[response]['response'] if 'นะค่ะ' in res] == []
