6c23656765a6ee50d291f7717eb1e6dc3e84ddcd
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2022-09-07 10:59:14 +0700

    Add uwsgi config for API hosting

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def send_response():
     
 
 if __name__ == '__main__':
-    app.run(debug = True, host = '0.0.0.0', port = int(os.environ.get('PORT', 8080)))
+    app.run(host = '0.0.0.0', debug = True)
