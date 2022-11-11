78d1f65f7ca5352de326be5d95555f82227d746d
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2022-11-11 21:49:08 +0700

    Update Header Styling

--- web/src/components/Navbar/Navbar.tsx
+++ web/src/components/Navbar/Navbar.tsx
@@ -11,28 +11,28 @@ const Navbar = () => {
         <nav>
 
             <img 
-                src="https://cdn.discordapp.com/app-icons/927573556961869825/b4b624c1cb68fa3a99a24a8e9942d2a5.png" 
-                alt="image" 
+                src='https://cdn.discordapp.com/app-icons/927573556961869825/b4b624c1cb68fa3a99a24a8e9942d2a5.png' 
+                alt='image' 
                 className='logo'
             />
 
-            <div className="nav-container">
+            <div className='nav-container'>
 
                 <ul className={click ? 'nav-list-active' : 'nav-list'}>
 
                     <li>
-                        <a href="#header">Home</a>
+                        <a href='https://discord.com/api/oauth2/authorize?client_id=927573556961869825&permissions=414464863296&scope=bot'>Invite</a>
                     </li>
                     
                     <li>
-                        <a href="#demo">Live demo</a>
+                        <a href='#demo'>Live demo</a>
                     </li>
 
                 </ul>
 
             </div>
 
-            <div className="nav-moblie" onClick={() => setClick(!click)}>
+            <div className='nav-moblie' onClick={() => setClick(!click)}>
                 <i className={ click ? 'fas fa-times' : 'fas fa-bars'}></i>
             </div>
 
