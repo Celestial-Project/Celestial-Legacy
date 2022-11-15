cf2180a2714c8b78527cc8292c71f3a269cd1826
Jirayu Kaewsing <strixz.self@gmail.com>
2022-11-15 16:04:55 +0700

    Update Header Styling

--- web/src/components/Header/DoubleWaves.tsx
+++ web/src/components/Header/DoubleWaves.tsx
@@ -4,7 +4,7 @@ const DoubleWaves = () => {
   return (
     <>
         <img src={Waves} alt='waves' className='waves' />
-        <img src={Waves} alt='waves' className='rotate-180 -mt-10'/>
+        <img src={Waves} alt='waves' className='rotate-180 -mt-10 mix-blend-lighten max-md:-mt-1'/>
     </>
   )
 }
