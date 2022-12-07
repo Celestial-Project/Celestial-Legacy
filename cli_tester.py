a3444ecaf0610f4363ee2ab9348754eccd769ee8
Jirayu Kaewsing <strixz.self@gmail.com>
2022-12-07 22:13:20 +0700

    Refactor cli_tester.py to reduce nested code

--- cli_tester.py
+++ cli_tester.py
@@ -8,27 +8,29 @@ print('\u001b[45;1m ** \u001b[0m Welcome to Celestial command-line testing inter
 print('\u001b[45;1m ** \u001b[0m Type -h or --help to see list of test macro and controls.')
 print('\u001b[45;1m ** \u001b[0m Press ctrl+c to exit.')
 
+def read_input(message: str) -> None:
+
+    if message in {'--help', '-h'}:
+        print('''
+            \u001b[43;1m macro \u001b[0m -h --help \t\t Show this help message.
+            \u001b[43;1m macro \u001b[0m -r --reload \t Reload the chat module.
+            \u001b[41;1m  ctl  \u001b[0m ^C ctrl+c \t\t Quit the program.
+        ''')
+
+    elif message in {'--reload', '-r'}:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print('\u001b[45;1m ** \u001b[0m Reloading...')
+        reload(chat_response)
+                  
+    else:
+        chat_response.get_response(message, debug = True)
+
+
 while True:
     
     try:
-        
         inp = input('\u001b[47;1m #> \u001b[0m ')
-        
-        if inp in {'--help', '-h'}:
-            print('''
-                  \u001b[43;1m macro \u001b[0m -h --help \t Show this help message.
-                  \u001b[43;1m macro \u001b[0m -r --reload \t Reload the chat module.
-                  \u001b[41;1m  ctl  \u001b[0m ^C ctrl+c \t Quit the program.
-                  ''')
-        
-        # Reload the engine on command
-        elif inp in {'--reload', '-r'}:
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print('\u001b[45;1m ** \u001b[0m Reloading...')
-            reload(chat_response)
-                  
-        else:
-            chat_response.get_response(inp, debug = True)
+        read_input(inp)
         
     except KeyboardInterrupt:
         print('\n\u001b[45;1m ** \u001b[0m Exiting test mode...')
