0727e27906eaccb63ecb8eeb0d84efb538ac82d1
Jirayu Kaewsing <strixz.self@gmail.com>
2022-12-07 21:41:12 +0700

    Update margin top for smaller screen

--- web/src/components/Header/Scrolldown.tsx
+++ web/src/components/Header/Scrolldown.tsx
@@ -4,9 +4,9 @@ import Waves from '../../assets/layered-waves-haikei.svg'
 const Scrolldown = () => {
   return (
 
-    <div className='mt-48'>
+    <div className='mt-28'>
 
-        <div className='container pt-5 pb-1 px-10 mx-0 min-w-full flex flex-col items-center mt-36 z-1 absolute'>
+        <div className='container pt-5 pb-1 px-10 mx-0 min-w-full flex flex-col items-center mt-28 z-1 absolute'>
             <a href='#demo' className='p-8'>
                 <FaArrowDown className='bg-[#464646] h-12 w-12 p-2 rounded-full'/>
             </a>
