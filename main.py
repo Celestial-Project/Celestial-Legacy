f12b4dbe97e1686e67690d5dd24115a54f59860f
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2023-01-01 15:14:47 +0700

    Fix moderator ID check bug

--- main.py
+++ main.py
@@ -142,6 +142,6 @@ async def on_pull_error(ctx: commands.Context, error: commands.errors) -> None:
 if __name__ == '__main__':
 
     load_dotenv()
-    moderator_ids = set([ids for (key, ids) in os.environ.items() if key.startswith('ID')])
+    moderator_ids = set([int(ids) for (key, ids) in os.environ.items() if key.startswith('ID')])
     
     client.run(os.getenv('TOKEN'))
