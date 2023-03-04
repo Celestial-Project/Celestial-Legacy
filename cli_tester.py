8a943158c04671eb1b5e1e75c1c953580b480650
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2023-03-05 00:27:49 +0700

    Update import path

--- cli_tester.py
+++ cli_tester.py
@@ -2,7 +2,7 @@ import os
 import chat_response
 
 from importlib import reload
-from utils.logger import info_log
+from logger import info_log
 
 # run this file to test your chat intents on the terminal before commit
 
