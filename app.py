066411e51fbac1495cbe00bfc8d600f997b4ab5a
Jirayu Kaewsing <strixz.self@gmail.com>
2023-04-08 23:42:14 +0700

    Adding API ready message

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+from logger import info_log
 from chat_response import get_response
 
 debug = False
@@ -13,6 +15,16 @@ cors = CORS(app)
 limiter = Limiter(key_func = get_remote_address, app = app)
 
 app.config['MAX_CONTENT_LENGTH'] = 1024
+
+def show_ready(port: int, debug: bool) -> None:
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    info_log('Chat REST API ready!')
+    info_log(f'Mode: {"Debug" if debug else "Production"}')
+    info_log('Press ctrl+c to exit.')
+    info_log(f'API running on: http://localhost:{port}')
+
 
 @cross_origin()
 @limiter.limit('450/minute')
@@ -29,12 +41,19 @@ def send_response():
 
 def main():
 
-    if debug:   
-        app.run(host = '0.0.0.0', port = 21250)
+    PORT = 21250
+
+    if debug:  
+
+        show_ready(PORT, debug)
+
+        app.run(host = '0.0.0.0', port = PORT)
         return
 
     from waitress import serve
-    serve(app, host = '0.0.0.0', port = 21250)
+    
+    show_ready(PORT, debug)
+    serve(app, host = '0.0.0.0', port = PORT)
     
 
 if __name__ == '__main__':
