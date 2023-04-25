b0f6892a0131fedb04565d36e5435155e5041e93
Jirayu Kaewsing <strixz.self@gmail.com>
2023-04-25 20:47:02 +0700

    Update startup status message

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def show_ready(port: int, debug: bool) -> None:
     info_log('Chat REST API ready!')
     info_log(f'Mode: {"Debug" if debug else "Production"}')
     info_log('Press ctrl+c to exit.')
-    info_log(f'API running on: http://localhost:{port}')
+    info_log(f'API running on: http://localhost:{port}/chat-legacy')
 
 
 @cross_origin()
